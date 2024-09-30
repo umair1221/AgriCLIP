a1409829967469d1ae6264ff1e87151b1e104d22
umair1221 <umair.nawaz@mbzuai.ac.ae>
2024-09-30 22:24:19 +0400

    Added prompts

---
+++ AgriCLIP_alignment/prompts.py
@@ -0,0 +1,19 @@
+{
+ 'Acquarium Fish': 1,
+ 'Animal-2': 2,
+ 'Banana Deficiency': 3,
+ 'Bean Lesion': 4,
+ 'Chicken Fecus': 5,
+ 'CID': 6,
+ 'Citrus Fruits': 7,
+ 'Citrus Leaves': 8,
+ 'Cow Breed': 9,
+ 'DeepFish': 10,
+ 'Fish Freshness': 11,
+ 'Fish Species': 12,
+ 'FishNet': 13,
+ 'Fruits Diseases': 14,
+ 'Horse Breed': 15,
+ 'Soybean Weeds': 16,
+ 'Supermarket Fish': 17
+}
